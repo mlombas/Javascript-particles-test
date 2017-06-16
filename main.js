a681c66c3d24cd83beef384c848fce73ebe80c1e
Mocoma <mocomaminecraft@gmail.com>
2017-06-16 19:15:53 +0200

    Changed the objects to actul classes

--- main.js
+++ main.js
@@ -1,22 +1,23 @@
 //Variable declarations
 var canvas;
-var force, resistance; //resistance in % of movement
 var mouse, mouseIsDown;
-var maxParticles = 1, particles = [maxParticles]; //particles is an array
+var force, resistance; //resistance in % of movement
+var maxParticles = 5, particles = [maxParticles]; //particles is an array
 //End of variable declarations
-//Object constructors
-function particle() {
-	this.radius = 5;
-	this.pos = new vec(Math.random() * (canvas.width - 1), Math.random() * (canvas.height - 1));
+//Classes definitions
+class particle {
+	constructor(x,y) {
+	this.radius = 0.3;
+	this.pos = new vec(x, y);
 	this.f = new vec(0,0); 
-	this.draw = function(g) {
+	}
+	draw(g) {
 		g.fillStyle = "#c47a35";
 		g.fillRect(this.pos.x - this.radius, this.pos.y - this.radius,2 * this.radius, 2 * this.radius);
 	}
-	this.update = function(mousePos) {
+	update(mousePos) {
 		if(!mouseIsDown) return;
-		var toMouse = new vec(mousePos.x - this.pos.x, mousePos.y - this.pos.y);
-		console.log(mousePos.x + " " + mousePos.y + "  " + toMouse.x + " " + toMouse.y);		
+		var toMouse = new vec(mousePos.x - this.pos.x, mousePos.y - this.pos.y);		
 		toMouse.normalize();
 		this.f.x += toMouse.x * force - this.f.x * resistance;
 		this.f.y += toMouse.y * force - this.f.y * resistance;
@@ -27,16 +28,18 @@ function particle() {
 	}
 }
 
-function vec(x,y) {
-	this.x = x;
-	this.y = y;
-	this.length = function() { Math.sqrt(x*x + y*y); }
-	this.normalize = function() {
+class vec {
+	constructor(x, y) {
+		this.x = x;
+		this.y = y;
+	}
+	length(){ Math.sqrt(this.x * this.x + this.y * this.y); }
+	normalize(){
 		this.x /= this.length();
 		this.y /= this.length();
 	}
 }
-//End of object constructors
+//End of classes definitions
 
 function start() {
 	canvas = document.getElementById("particleField"), gfx = canvas.getContext("2d");
@@ -47,7 +50,7 @@ function start() {
 	canvas.onmousedown = function(e){mouseIsDown = true; mouse = getMousePos(canvas, e)};
 	canvas.onmouseup = function(e){mouseIsDown = false};
 	canvas.onmousemove = function(e){if(mouseIsDown) {mouse = getMousePos(canvas, e)} };
-	for(var i = 0; i < maxParticles; i++) particles[i] = new particle();
+	for(var i = 0; i < maxParticles; i++) particles[i] = new particle(Math.random() * (canvas.width - 1), Math.random() * (canvas.height - 1));
 	var loop = setInterval(mainLoop, 16);
 }
 
