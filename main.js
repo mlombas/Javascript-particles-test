e05f1a671ef7ad6043f9356b66ca8c3d800e9974
Mocoma <mocomaminecraft@gmail.com>
2017-06-18 10:40:09 +0200

    Some code improvements

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
 //Variable declarations
 var canvas;
 var mouse, mouseIsDown;
-var force, resistance; //resistance in % of movement
+var force, resistance, particleRadius; //resistance in % of movement
 var maxParticles, particles; //particles is an array
 var loop;
 //End of variable declarations
 //Classes definitions
 class particle {
-	constructor(x,y) {
-		this.radius = 0.5;
+	constructor(x, y, r) {
+		this.radius = r;
 		this.pos = new vec(x, y);
 		this.f = new vec(0,0); 
 	}
@@ -16,7 +16,7 @@ class particle {
 		g.fillStyle = "#c47a35";
 		g.fillRect(this.pos.x - this.radius, this.pos.y - this.radius,2 * this.radius, 2 * this.radius);
 	}
-	update(mousePos) {
+	update(mousePos, force, resistance) {
 		if(mouseIsDown){
 			var toMouse = new vec(mousePos.x - this.pos.x, mousePos.y - this.pos.y);	
 			toMouse.normalize();	
@@ -53,13 +53,17 @@ function reset() {
 	force = document.getElementById("force").value;
 	resistance = document.getElementById("resistance").value;
 	maxParticles = document.getElementById("particles").value;
+	particleRadius = document.getElementById("particleRadius").value
 	particles = [maxParticles];
-	for(var i = 0; i < maxParticles; i++) particles[i] = new particle(Math.random() * (canvas.width - 1), Math.random() * (canvas.height - 1));
+	for(var i = 0; i < maxParticles; i++) particles[i] = new particle(Math.random() * (canvas.width - 1), Math.random() * (canvas.height - 1), particleRadius);
 	loop = setInterval(mainLoop, 16);
 }
 
 function start() {
 	canvas = document.getElementById("particleField"), gfx = canvas.getContext("2d");
+	canvas.width = 8*screen.width/10;
+	canvas.height = 8*screen.height/10;
+	console.log("hey");
 	mouse = new vec(0,0);
 	mouseIsDown = false;
 	canvas.onmousedown = function(e){mouseIsDown = true; mouse = getMousePos(canvas, e)};
@@ -74,7 +78,7 @@ function mainLoop() {
 }
 
 function update() {
-	for(var i = 0; i < maxParticles; i++) particles[i].update(mouse);
+	for(var i = 0; i < maxParticles; i++) particles[i].update(mouse, force, resistance);
 }
 
 function draw() {
@@ -86,6 +90,5 @@ function  getMousePos(canvas, evt) {
   var rect = canvas.getBoundingClientRect(),
       scaleX = canvas.width / rect.width,
       scaleY = canvas.height / rect.height;
-
   return new vec((evt.clientX - rect.left) * scaleX, (evt.clientY - rect.top) * scaleY );
 }
